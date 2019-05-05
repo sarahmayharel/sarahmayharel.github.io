e75dd8f80108fb03b21b0d4447b97196430c66bb
sarahmayharel <32714752+sarahmayharel@users.noreply.github.com>
2019-05-05 10:35:26 -0400

    initialize firebase and push reservation data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 40.8054491, lng: -73.9654415},
-		zoom:8,
-		scrollwheel: false
-	});
-}
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBNiZlPfVQDbgFhgR6snJWPGsmOeMz7duc",
+    authDomain: "reservation-site-993f5.firebaseapp.com",
+    databaseURL: "https://reservation-site-993f5.firebaseio.com",
+    projectId: "reservation-site-993f5",
+    storageBucket: "reservation-site-993f5.appspot.com",
+    messagingSenderId: "1099093752818",
+    appId: "1:1099093752818:web:f7ef40253e2a7d1b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  var reservationData = {};
+
+  // set the day when an option is clicked on
+$('.reservation-day li').click(function() {
+  reservationData.day = $(this).text();
+});
+
+$('.reservations').on('submit', function(e) {
+  e.preventDefault();
+
+  reservationData.name = $('.reservation-name').val();
+
+  database.ref('reservations').push(reservationData);
+});
