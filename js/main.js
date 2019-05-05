89abe73a8eb3398b1c425a565eafb375673d3fe3
sarahmayharel <32714752+sarahmayharel@users.noreply.github.com>
2019-05-05 11:32:08 -0400

    updates view with reservation data, adds map

--- js/main.js
+++ js/main.js
@@ -27,3 +27,37 @@ $('.reservations').on('submit', function(e) {
 
   database.ref('reservations').push(reservationData);
 });
+
+// on initial load and addition of each reservation update the view
+database.ref('reservations').on('child_added', function(snapshot) {
+  // grab element to hook to
+  var reservationList = $('.reservation-list');
+  // get data from database
+  var reservations = snapshot.val();
+  // get your template from your script tag
+  var source   = $("#reservation-template").html();
+  // compile template
+  var template = Handlebars.compile(source);
+  // pass data to template to be evaluated within handlebars
+  // as the template is created
+  var reservationTemplate = template(reservations);
+  // append created templated
+  reservationList.append(reservationTemplate);
+});
+
+// initialize the configuration of map
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7128, lng: -74.0059},
+    zoom: 10,
+    scrollwheel: false,
+    styles: [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
+  });
+
+  // use Marker constructor to add a marker to map
+  var marker = new google.maps.Marker({
+    position: {lat: 40.7128, lng: -74.0059},
+    map: map,
+    title: 'Monks Café'
+  });
+}
